e2b235dea63bb6fed5e4f36779bbba59d5112c11
Stuart Jameson <sjameson@fullfacing.com>
2019-02-26 15:09:41 +0200

    AF-67 #time 3h Finished the Users service

--- src/main/scala/com/fullfacing/keycloak4s/services/package.scala
+++ src/main/scala/com/fullfacing/keycloak4s/services/package.scala
@@ -3,6 +3,9 @@ package com.fullfacing.keycloak4s
 import com.softwaremill.sttp.Uri.QueryFragment.KeyValue
 
 package object services {
+
+  trait TODO extends Nothing
+
   def createQuery(queries: (String, Option[Any])*): Seq[KeyValue] = {
     queries.flatMap { case (key, value) =>
       value.map(v => KeyValue(key, v.toString))
