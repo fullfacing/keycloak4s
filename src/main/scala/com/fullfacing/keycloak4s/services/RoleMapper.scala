bd89fbcd099191b0e95ce05e790cec9cf01b3067
Stuart Jameson <sjameson@fullfacing.com>
2019-03-08 09:32:26 +0200

    Updated services according to Richard's new abstract KeycloakClient

--- src/main/scala/com/fullfacing/keycloak4s/services/RoleMapper.scala
+++ src/main/scala/com/fullfacing/keycloak4s/services/RoleMapper.scala
@@ -1,162 +1,161 @@
-//package com.fullfacing.keycloak4s.services
-//
-//import com.fullfacing.apollo.core.Predef.AsyncApolloResponse
-//import com.fullfacing.apollo.core.protocol.NoContent
-//import com.fullfacing.keycloak4s.client.KeycloakClient
-//import com.fullfacing.keycloak4s.models.{Mappings, Role}
-//import com.softwaremill.sttp.Uri.QueryFragment.KeyValue
-//
-//import scala.collection.immutable.Seq
-//
-//object RoleMapper {
-//
-//  private val groups_path   = "groups"
-//  private val role_mappings = "role-mappings"
-//
-//  /**
-//   *
-//   * @param realm realm name (not id!)
-//   * @param id
-//   * @return
-//   */
-//  def fetchRoleMappings(realm: String, id: String)(implicit authToken: String): AsyncApolloResponse[Mappings] = {
-//    val path = Seq(realm, groups_path, id, role_mappings)
-//    SttpClient.get(path)
-//  }
-//
-//  /**
-//   * Add realm-level role mappings to the user
-//   *
-//   * @param realm realm name (not id!)
-//   * @param id
-//   * @param roles Roles to add
-//   * @return
-//   */
-//  def addRealmLevelRoleMappings(realm: String, id: String, roles: String)(implicit authToken: String): AsyncApolloResponse[NoContent] = {
-//    val path = Seq(realm, groups_path, id, role_mappings, "realm")
-//    SttpClient.post(roles, path)
-//  }
-//
-//  /**
-//   * Get realm-level role mappings
-//   *
-//   * @param realm
-//   * @param id
-//   * @return
-//   */
-//  def fetchRealmRoleMappings(realm: String, id: String)(implicit authToken: String): AsyncApolloResponse[List[Role]] = {
-//    val path = Seq(realm, groups_path, id, role_mappings, "realm")
-//    SttpClient.get(path)
-//  }
-//
-//  /**
-//   * Delete realm-level role mappings
-//   *
-//   * @param realm realm name (not id!)
-//   * @param id
-//   * @param roles Roles to be deleted
-//   * @return
-//   */
-//  def removeRealmRoleMappings(realm: String, id: String, roles: List[Role])(implicit authToken: String): AsyncApolloResponse[NoContent] = {
-//    val path = Seq(realm, groups_path, id, role_mappings, "realm")
-//    SttpClient.delete(roles, path, Seq.empty[KeyValue])
-//  }
-//
-//  /**
-//   * Get realm-level roles that can be mapped
-//   *
-//   * @param realm realm name (not id!)
-//   * @param id
-//   * @return
-//   */
-//  def getAvailableRealmRoles(realm: String, id: String)(implicit authToken: String): AsyncApolloResponse[List[Role]] = {
-//    val path = Seq(realm, groups_path, id, role_mappings, "realm", "available")
-//    SttpClient.get(path)
-//  }
-//
-//  /**
-//   * Get effective realm-level role mappings This will recurse all composite roles to get the result.
-//   *
-//   * @param realm realm name (not id!)
-//   * @param id
-//   * @return
-//   */
-//  def getEffectiveRealmRoleMappings(realm: String, id: String)(implicit authToken: String): AsyncApolloResponse[List[Role]] = {
-//    val path = Seq(realm, groups_path, id, role_mappings, "realm", "composite")
-//    SttpClient.get(path)
-//  }
-//
-//  /**
-//   * Get role mappings
-//   *
-//   * @param realm  realm name (not id!)
-//   * @param userId User id
-//   * @return
-//   */
-//  def getUserRoleMappings(realm: String, userId: String)(implicit authToken: String): AsyncApolloResponse[Mappings] = {
-//    val path = Seq(realm, "users", userId, role_mappings)
-//    SttpClient.get(path)
-//  }
-//
-//  /**
-//   * Add realm-level role mappings to the user
-//   *
-//   * @param realm  realm name (not id!)
-//   * @param userId User id
-//   * @param roles  Roles to add
-//   * @return
-//   */
-//  def addRealmRoleMappingsToUser(realm: String, userId: String, roles: String)(implicit authToken: String): AsyncApolloResponse[List[Role]] = {
-//    val path = Seq(realm, "users", userId, role_mappings, "realm")
-//    SttpClient.post(roles, path)
-//  }
-//
-//  /**
-//   * Get realm-level role mappings
-//   *
-//   * @param realm  realm name (not id!)
-//   * @param userId User id
-//   * @return
-//   */
-//  def getUserRealmRoleMappings(realm: String, userId: String)(implicit authToken: String): AsyncApolloResponse[List[Role]] = {
-//    val path = Seq(realm, "users", userId, role_mappings, "realm")
-//    SttpClient.get(path)
-//  }
-//
-//  /**
-//   * Delete realm-level role mappings
-//   *
-//   * @param realm  realm name (not id!)
-//   * @param userId User id
-//   * @param roles  Roles to be deleted
-//   * @return
-//   */
-//  def removeUserRealmRoleMappings(realm: String, userId: String, roles: List[Role])(implicit authToken: String): AsyncApolloResponse[NoContent] = {
-//    val path = Seq(realm, "users", userId, role_mappings, "realm")
-//    SttpClient.delete(roles, path, Seq.empty[KeyValue])
-//  }
-//
-//  /**
-//   * Get realm-level roles that can be mapped
-//   *
-//   * @param realm  realm name (not id!)
-//   * @param userId User id
-//   * @return
-//   */
-//  def getAvailableUserRealmRoles(realm: String, userId: String)(implicit authToken: String): AsyncApolloResponse[List[Role]] = {
-//    val path = Seq(realm, "users", userId, role_mappings, "realm", "available")
-//    SttpClient.get(path)
-//  }
-//
-//  /**
-//   * Get effective realm-level role mappings. This will recurse all composite roles to get the result.
-//   *
-//   * @param realm  realm name (not id!)
-//   * @param userId User id
-//   * @return
-//   */
-//  def getEffectiveUserRealmRoles(realm: String, userId: String)(implicit authToken: String): AsyncApolloResponse[List[Role]] = {
-//    val path = Seq(realm, "users", userId, role_mappings, "realm", "available")
-//    SttpClient.get(path)
-//  }
-//}
+package com.fullfacing.keycloak4s.services
+
+import cats.effect.Effect
+import com.fullfacing.keycloak4s.client.KeycloakClient
+import com.fullfacing.keycloak4s.models.{Mappings, Role}
+import com.softwaremill.sttp.Uri.QueryFragment.KeyValue
+
+import scala.collection.immutable.Seq
+
+class RoleMapper[R[_]: Effect, S](implicit client: KeycloakClient[R, S]) {
+
+  private val groups_path   = "groups"
+  private val role_mappings = "role-mappings"
+
+  /**
+   *
+   * @param realm realm name (not id!)
+   * @param id
+   * @return
+   */
+  def fetchRoleMappings(realm: String, id: String): R[Mappings] = {
+    val path = Seq(realm, groups_path, id, role_mappings)
+    client.get[Mappings](path)
+  }
+
+  /**
+   * Add realm-level role mappings to the user
+   *
+   * @param realm realm name (not id!)
+   * @param id
+   * @param roles Roles to add
+   * @return
+   */
+  def addRealmLevelRoleMappings(realm: String, id: String, roles: List[Role]): R[Unit] = {
+    val path = Seq(realm, groups_path, id, role_mappings, "realm")
+    client.post[List[Role], Unit](roles, path)
+  }
+
+  /**
+   * Get realm-level role mappings
+   *
+   * @param realm
+   * @param id
+   * @return
+   */
+  def fetchRealmRoleMappings(realm: String, id: String): R[List[Role]] = {
+    val path = Seq(realm, groups_path, id, role_mappings, "realm")
+    client.get[List[Role]](path)
+  }
+
+  /**
+   * Delete realm-level role mappings
+   *
+   * @param realm realm name (not id!)
+   * @param id
+   * @param roles Roles to be deleted
+   * @return
+   */
+  def removeRealmRoleMappings(realm: String, id: String, roles: List[Role]): R[Unit] = {
+    val path = Seq(realm, groups_path, id, role_mappings, "realm")
+    client.delete[List[Role], Unit](roles, path, Seq.empty[KeyValue])
+  }
+
+  /**
+   * Get realm-level roles that can be mapped
+   *
+   * @param realm realm name (not id!)
+   * @param id
+   * @return
+   */
+  def getAvailableRealmRoles(realm: String, id: String): R[List[Role]] = {
+    val path = Seq(realm, groups_path, id, role_mappings, "realm", "available")
+    client.get[List[Role]](path)
+  }
+
+  /**
+   * Get effective realm-level role mappings This will recurse all composite roles to get the result.
+   *
+   * @param realm realm name (not id!)
+   * @param id
+   * @return
+   */
+  def getEffectiveRealmRoleMappings(realm: String, id: String): R[List[Role]] = {
+    val path = Seq(realm, groups_path, id, role_mappings, "realm", "composite")
+    client.get[List[Role]](path)
+  }
+
+  /**
+   * Get role mappings
+   *
+   * @param realm  realm name (not id!)
+   * @param userId User id
+   * @return
+   */
+  def getUserRoleMappings(realm: String, userId: String): R[Mappings] = {
+    val path = Seq(realm, "users", userId, role_mappings)
+    client.get[Mappings](path)
+  }
+
+  /**
+   * Add realm-level role mappings to the user
+   *
+   * @param realm  realm name (not id!)
+   * @param userId User id
+   * @param roles  Roles to add
+   * @return
+   */
+  def addRealmRoleMappingsToUser(realm: String, userId: String, roles: List[Role]): R[Unit] = {
+    val path = Seq(realm, "users", userId, role_mappings, "realm")
+    client.post[List[Role], Unit](roles, path)
+  }
+
+  /**
+   * Get realm-level role mappings
+   *
+   * @param realm  realm name (not id!)
+   * @param userId User id
+   * @return
+   */
+  def getUserRealmRoleMappings(realm: String, userId: String): R[List[Role]] = {
+    val path = Seq(realm, "users", userId, role_mappings, "realm")
+    client.get[List[Role]](path)
+  }
+
+  /**
+   * Delete realm-level role mappings
+   *
+   * @param realm  realm name (not id!)
+   * @param userId User id
+   * @param roles  Roles to be deleted
+   * @return
+   */
+  def removeUserRealmRoleMappings(realm: String, userId: String, roles: List[Role]): R[Unit] = {
+    val path = Seq(realm, "users", userId, role_mappings, "realm")
+    client.delete[List[Role], Unit](roles, path, Seq.empty[KeyValue])
+  }
+
+  /**
+   * Get realm-level roles that can be mapped
+   *
+   * @param realm  realm name (not id!)
+   * @param userId User id
+   * @return
+   */
+  def getAvailableUserRealmRoles(realm: String, userId: String): R[List[Role]] = {
+    val path = Seq(realm, "users", userId, role_mappings, "realm", "available")
+    client.get[List[Role]](path)
+  }
+
+  /**
+   * Get effective realm-level role mappings. This will recurse all composite roles to get the result.
+   *
+   * @param realm  realm name (not id!)
+   * @param userId User id
+   * @return
+   */
+  def getEffectiveUserRealmRoles(realm: String, userId: String): R[List[Role]] = {
+    val path = Seq(realm, "users", userId, role_mappings, "realm", "available")
+    client.get[List[Role]](path)
+  }
+}
--- src/main/scala/com/fullfacing/keycloak4s/services/ScopeMappings.scala
+++ src/main/scala/com/fullfacing/keycloak4s/services/ScopeMappings.scala
@@ -1,160 +1,159 @@
-//package com.fullfacing.keycloak4s.services
-//
-//import com.fullfacing.apollo.core.Predef.AsyncApolloResponse
-//import com.fullfacing.apollo.core.protocol.NoContent
-//import com.fullfacing.keycloak4s.client.KeycloakClient
-//import com.fullfacing.keycloak4s.models.{Mappings, Role}
-//import com.softwaremill.sttp.Uri.QueryFragment.KeyValue
-//
-//import scala.collection.immutable.Seq
-//
-//object ScopeMappings {
-//
-//  private val resource = "client-scopes"
-//  private val mappings = "scope-mappings"
-//
-//  /**
-//   * Get all scope mappings for the client
-//   *
-//   * @param realm realm name (not id!)
-//   * @param id    id of client scope (not name)
-//   * @return
-//   */
-//  def fetchMappings(realm: String, id: String)(implicit authToken: String): AsyncApolloResponse[Mappings] = {
-//    val path = Seq(realm, resource, id, mappings)
-//    SttpClient.get(path)
-//  }
-//
-//  /**
-//   * Add client-level roles to the client’s scope
-//   *
-//   * @param realm realm name (not id!)
-//   * @param id    id of client scope (not name)
-//   * @param client
-//   * @param roles
-//   * @return
-//   */
-//  def addClientRoles(realm: String, id: String, client: String, roles: List[Role])(implicit authToken: String): AsyncApolloResponse[NoContent] = {
-//    val path = Seq(realm, resource, id, mappings, "clients", client)
-//    SttpClient.post(roles, path)
-//  }
-//
-//  /**
-//   * Get the roles associated with a client’s scope. Returns roles for the client.
-//   *
-//   * @param realm realm name (not id!)
-//   * @param id    id of client scope (not name)
-//   * @param client
-//   * @return
-//   */
-//  def getClientRoles(realm: String, id: String, client: String)(implicit authToken: String): AsyncApolloResponse[List[Role]] = {
-//    val path = Seq(realm, resource, id, mappings, "clients", client)
-//    SttpClient.get(path)
-//  }
-//
-//  /**
-//   * Remove client-level roles from the client’s scope.
-//   *
-//   * @param realm realm name (not id!)
-//   * @param id    id of client scope (not name)
-//   * @param client
-//   * @return
-//   */
-//  def removeClientRoles(realm: String, id: String, client: String, roles: List[Role])(implicit authToken: String): AsyncApolloResponse[NoContent] = { // TODO Test - Delete with body
-//    val path = Seq(realm, resource, id, mappings, "clients", client)
-//    SttpClient.delete(roles, path, Seq.empty[KeyValue])
-//  }
-//
-//  /**
-//   * The available client-level roles
-//   * Returns the roles for the client that can be associated with the client’s scope
-//   *
-//   * @param realm realm name (not id!)
-//   * @param id    id of client scope (not name)
-//   * @param client
-//   * @return
-//   */
-//  def getAvailableClientRoles(realm: String, id: String, client: String)(implicit authToken: String): AsyncApolloResponse[Role] = {
-//    val path = Seq(realm, resource, id, mappings, "clients", client, "available")
-//    SttpClient.get(path)
-//  }
-//
-//  /**
-//   * Get effective client roles
-//   * Returns the roles for the client that are associated with the client’s scope.
-//   *
-//   * @param realm realm name (not id!)
-//   * @param id    id of client scope (not name)
-//   * @param client
-//   * @return
-//   */
-//  def getEffectiveClientRoles(realm: String, id: String, client: String)(implicit authToken: String): AsyncApolloResponse[Role] = {
-//    val path = Seq(realm, resource, id, mappings, "clients", client, "composite")
-//    SttpClient.get(path)
-//  }
-//
-//  /**
-//   * Add a set of realm-level roles to the client’s scope
-//   *
-//   * @param realm realm name (not id!)
-//   * @param id    id of client scope (not name)
-//   * @param roles
-//   * @return
-//   */
-//  def addRealmRolesToClientScope(realm: String, id: String, roles: List[Role])(implicit authToken: String): AsyncApolloResponse[NoContent] = {
-//    val path = Seq(realm, resource, id, mappings, "realm")
-//    SttpClient.post(roles, path)
-//  }
-//
-//  /**
-//   * Get realm-level roles associated with the client’s scope
-//   *
-//   * @param realm realm name (not id!)
-//   * @param id    id of client scope (not name)
-//   * @return
-//   */
-//  def getClientScopeRealmRoles(realm: String, id: String)(implicit authToken: String): AsyncApolloResponse[List[Role]] = {
-//    val path = Seq(realm, resource, id, mappings, "realm")
-//    SttpClient.get(path)
-//  }
-//
-//  /**
-//   * Remove a set of realm-level roles from the client’s scope
-//   *
-//   * @param realm realm name (not id!)
-//   * @param id    id of client scope (not name)
-//   * @param roles
-//   * @return
-//   */
-//  def removeRealmRolesFromClientScope(realm: String, id: String, roles: List[Role])(implicit authToken: String): AsyncApolloResponse[NoContent] = {
-//    val path = Seq(realm, resource, id, mappings, "realm")
-//    SttpClient.delete(roles, path, Seq.empty[KeyValue])
-//  }
-//
-//  /**
-//   * Get realm-level roles that are available to attach to this client’s scope
-//   *
-//   * @param realm realm name (not id!)
-//   * @param id    id of client scope (not name)
-//   * @return
-//   */
-//  def getAvailableRealmLevelRoles(realm: String, id: String)(implicit authToken: String): AsyncApolloResponse[List[Role]] = {
-//    val path = Seq(realm, resource, id, mappings, "realm", "available")
-//    SttpClient.get(path)
-//  }
-//
-//  /**
-//   * Get effective realm-level roles associated with the client’s scope.
-//   * What this does is recurse any composite roles associated with the client’s scope and adds the roles to this lists.
-//   *
-//   * The method is really to show a comprehensive total view of realm-level roles associated with the client
-//   *
-//   * @param realm realm name (not id!)
-//   * @param id    id of client scope (not name)
-//   * @return
-//   */
-//  def getEffectiveRealmLevelRoles(realm: String, id: String)(implicit authToken: String): AsyncApolloResponse[List[Role]] = {
-//    val path = Seq(realm, resource, id, mappings, "realm", "composite")
-//    SttpClient.get(path)
-//  }
-//}
+package com.fullfacing.keycloak4s.services
+
+import cats.effect.Effect
+import com.fullfacing.keycloak4s.client.KeycloakClient
+import com.fullfacing.keycloak4s.models.{Mappings, Role}
+import com.softwaremill.sttp.Uri.QueryFragment.KeyValue
+
+import scala.collection.immutable.Seq
+
+class ScopeMappings[R[_]: Effect, S](implicit keycloakClient: KeycloakClient[R, S]) {
+
+  private val resource = "client-scopes"
+  private val mappings = "scope-mappings"
+
+  /**
+   * Get all scope mappings for the client
+   *
+   * @param realm realm name (not id!)
+   * @param id    id of client scope (not name)
+   * @return
+   */
+  def fetchMappings(realm: String, id: String): R[Mappings] = {
+    val path = Seq(realm, resource, id, mappings)
+    keycloakClient.get[Mappings](path)
+  }
+
+  /**
+   * Add client-level roles to the client’s scope
+   *
+   * @param realm realm name (not id!)
+   * @param id    id of client scope (not name)
+   * @param client
+   * @param roles
+   * @return
+   */
+  def addClientRoles(realm: String, id: String, client: String, roles: List[Role]): R[Unit] = {
+    val path = Seq(realm, resource, id, mappings, "clients", client)
+    keycloakClient.post[List[Role], Unit](roles, path)
+  }
+
+  /**
+   * Get the roles associated with a client’s scope. Returns roles for the client.
+   *
+   * @param realm realm name (not id!)
+   * @param id    id of client scope (not name)
+   * @param client
+   * @return
+   */
+  def getClientRoles(realm: String, id: String, client: String): R[List[Role]] = {
+    val path = Seq(realm, resource, id, mappings, "clients", client)
+    keycloakClient.get[List[Role]](path)
+  }
+
+  /**
+   * Remove client-level roles from the client’s scope.
+   *
+   * @param realm realm name (not id!)
+   * @param id    id of client scope (not name)
+   * @param client
+   * @return
+   */
+  def removeClientRoles(realm: String, id: String, client: String, roles: List[Role]): R[Unit] = { // TODO Test - Delete with body
+    val path = Seq(realm, resource, id, mappings, "clients", client)
+    keycloakClient.delete[List[Role], Unit](roles, path, Seq.empty[KeyValue])
+  }
+
+  /**
+   * The available client-level roles
+   * Returns the roles for the client that can be associated with the client’s scope
+   *
+   * @param realm realm name (not id!)
+   * @param id    id of client scope (not name)
+   * @param client
+   * @return
+   */
+  def getAvailableClientRoles(realm: String, id: String, client: String): R[Role] = {
+    val path = Seq(realm, resource, id, mappings, "clients", client, "available")
+    keycloakClient.get[Role](path)
+  }
+
+  /**
+   * Get effective client roles
+   * Returns the roles for the client that are associated with the client’s scope.
+   *
+   * @param realm realm name (not id!)
+   * @param id    id of client scope (not name)
+   * @param client
+   * @return
+   */
+  def getEffectiveClientRoles(realm: String, id: String, client: String): R[Role] = {
+    val path = Seq(realm, resource, id, mappings, "clients", client, "composite")
+    keycloakClient.get[Role](path)
+  }
+
+  /**
+   * Add a set of realm-level roles to the client’s scope
+   *
+   * @param realm realm name (not id!)
+   * @param id    id of client scope (not name)
+   * @param roles
+   * @return
+   */
+  def addRealmRolesToClientScope(realm: String, id: String, roles: List[Role]): R[Unit] = {
+    val path = Seq(realm, resource, id, mappings, "realm")
+    keycloakClient.post[List[Role], Unit](roles, path)
+  }
+
+  /**
+   * Get realm-level roles associated with the client’s scope
+   *
+   * @param realm realm name (not id!)
+   * @param id    id of client scope (not name)
+   * @return
+   */
+  def getClientScopeRealmRoles(realm: String, id: String): R[List[Role]] = {
+    val path = Seq(realm, resource, id, mappings, "realm")
+    keycloakClient.get[List[Role]](path)
+  }
+
+  /**
+   * Remove a set of realm-level roles from the client’s scope
+   *
+   * @param realm realm name (not id!)
+   * @param id    id of client scope (not name)
+   * @param roles
+   * @return
+   */
+  def removeRealmRolesFromClientScope(realm: String, id: String, roles: List[Role]): R[Unit] = {
+    val path = Seq(realm, resource, id, mappings, "realm")
+    keycloakClient.delete[List[Role], Unit](roles, path, Seq.empty[KeyValue])
+  }
+
+  /**
+   * Get realm-level roles that are available to attach to this client’s scope
+   *
+   * @param realm realm name (not id!)
+   * @param id    id of client scope (not name)
+   * @return
+   */
+  def getAvailableRealmLevelRoles(realm: String, id: String): R[List[Role]] = {
+    val path = Seq(realm, resource, id, mappings, "realm", "available")
+    keycloakClient.get[List[Role]](path)
+  }
+
+  /**
+   * Get effective realm-level roles associated with the client’s scope.
+   * What this does is recurse any composite roles associated with the client’s scope and adds the roles to this lists.
+   *
+   * The method is really to show a comprehensive total view of realm-level roles associated with the client
+   *
+   * @param realm realm name (not id!)
+   * @param id    id of client scope (not name)
+   * @return
+   */
+  def getEffectiveRealmLevelRoles(realm: String, id: String): R[List[Role]] = {
+    val path = Seq(realm, resource, id, mappings, "realm", "composite")
+    keycloakClient.get[List[Role]](path)
+  }
+}
