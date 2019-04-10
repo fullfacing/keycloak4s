7d869ec9e02ec336349fd110a34c4c3f2a1c15f3
Louis Muller <lmuller@fullfacing.com>
2019-04-10 14:07:42 +0200

    Massively simplified JwksCache using IO and AtomicReference, dropping Monix dependency in the process.

--- keycloak4s-adapters/akka-http/src/main/scala/com/fullfacing/keycloak4s/adapters/akka/http/JwksCache.scala
+++ keycloak4s-adapters/akka-http/src/main/scala/com/fullfacing/keycloak4s/adapters/akka/http/JwksCache.scala
@@ -1,52 +1,38 @@
 package com.fullfacing.keycloak4s.adapters.akka.http
 
 import java.net.URL
+import java.util.concurrent.atomic.AtomicReference
 
+import cats.effect.IO
 import cats.implicits._
 import com.nimbusds.jose.jwk.JWKSet
-import monix.eval.Task
-import monix.execution.Scheduler
-import monix.execution.atomic.Atomic
 
-import scala.concurrent.Future
+import scala.concurrent.ExecutionContext
+import scala.util.control.NonFatal
 
-/**
- * A wrapper to retrieve and cache the JWK set from the Keycloak server with functionality to recache when necessary.
- *
- * Based off code from Alexandru Nedelcu and Michał Siatkowski.
- * https://github.com/monix/monix/issues/606
- */
-abstract class JwksCache (host: String, port: String, realm: String)(implicit s: Scheduler) {
-  type JwksFuture = Future[Either[Throwable, JWKSet]]
+abstract class JwksCache (host: String, port: String, realm: String)(implicit ec: ExecutionContext) {
 
   /* The URL to retrieve the ConnectID JWKS. **/
   private val url = new URL(s"http://$host:$port/auth/realms/$realm/protocol/openid-connect/certs")
 
-  /* The asynchronous Task to retrieve the JWKSet. **/
-  private val task = Task.evalAsync {
-    JWKSet.load(url).asRight[Throwable]
-  }.onErrorHandle(_.asLeft)
+  /* The cached JWK set. **/
+  private val ref: AtomicReference[Either[Throwable, JWKSet]] = new AtomicReference()
 
-  /* The cached Future resulting from executing the JWKSet retrieval Task. **/
-  private val future = Atomic(null: JwksFuture)
+  /* Retrieves the JWK set asynchronously and (re)caches it. **/
+  def recache(): IO[Either[Throwable, JWKSet]] = IO.async[JWKSet] { cb =>
+    ec.execute(() =>
+      try {
+        val jkws = JWKSet.load(url).asRight[Throwable]
+        ref.set(jkws)
+        cb(jkws)
+      } catch { case NonFatal(ex) =>
+        ref.set(ex.asLeft)
+        cb(ex.asLeft)
+      })
+  }.map(_.asRight[Throwable]).handleError(_.asLeft)
 
-  /* Executes the JWKSet retrieval Task, caches the resulting Future and returns it. **/
-  private def updateAndGet(): JwksFuture = synchronized {
-    future.set(task.runToFuture)
-    future.get
-  }
-
-  /* The Task containing the currently cached JWKSet. **/
-  val keySet: Task[Either[Throwable, JWKSet]] = Task.deferFuture {
-    future.get match {
-      case null   => updateAndGet()
-      case valid  => valid
-    }
-  }
-
-  /* Drops the cache, re-executes the JWKSet retrieval Task and returns the result. **/
-  def reobtainKeys(): Task[Either[Throwable, JWKSet]] = {
-    future.set(null: JwksFuture)
-    keySet
-  }
+  /* Retrieves the cached value. Recaches if empty. **/
+  def getCachedValue()(implicit e: ExecutionContext): IO[Either[Throwable, JWKSet]] = IO {
+    Option(ref.get).fold(recache())(IO(_))
+  }.flatten
 }
--- keycloak4s-adapters/akka-http/src/test/scala/com/fullfacing/keycloak4s/adapters/akka/http/Main.scala
+++ keycloak4s-adapters/akka-http/src/test/scala/com/fullfacing/keycloak4s/adapters/akka/http/Main.scala
@@ -2,14 +2,8 @@ package com.fullfacing.keycloak4s.adapters.akka.http
 
 import cats.effect.ExitCode
 import monix.eval.{Task, TaskApp}
-import monix.execution.Scheduler
-import org.json4s.Formats
 
 object Main extends TaskApp {
-  implicit val formats: Formats = org.json4s.DefaultFormats
-  implicit val s: Scheduler = monix.execution.Scheduler.global
 
-  override def run(args: List[String]): Task[ExitCode] = {
-    Task.eval(ExitCode.Success)
-  }.onErrorHandle{ex => ex.printStackTrace(); ExitCode.Error}
+  def run(args: List[String]): Task[ExitCode] = Akka.connect().map(_ => ExitCode.Success)
 }
