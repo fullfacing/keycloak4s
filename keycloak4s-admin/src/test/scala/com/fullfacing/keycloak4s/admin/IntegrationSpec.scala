a68614d3a376829a3c89d0fdcad345d01a8db1c7
Louis Muller <lmuller@fullfacing.com>
2019-05-24 10:09:11 +0200

    Finished integration tests for Users.

---
+++ keycloak4s-admin/src/test/scala/com/fullfacing/keycloak4s/admin/IntegrationSpec.scala
@@ -0,0 +1,32 @@
+package com.fullfacing.keycloak4s.admin
+
+import cats.effect.{ContextShift, IO}
+import com.fullfacing.keycloak4s.admin.client.{Keycloak, KeycloakClient}
+import com.fullfacing.keycloak4s.admin.services.{Groups, IdentityProviders, Users}
+import com.fullfacing.keycloak4s.core.models.{KeycloakConfig, KeycloakError}
+import com.softwaremill.sttp.SttpBackend
+import org.scalatest._
+
+import scala.concurrent.ExecutionContext.global
+
+class TestBase extends AsyncFlatSpec with Matchers with Inspectors {
+  val clientSecret: String = "ed66f43c-2434-4380-b067-ffb4253eea03" //ServerInitializer.clientSecret.unsafeRunSync()
+
+  /* Keycloak Server Configuration **/
+  val authConfig = KeycloakConfig.Auth("master", "admin-cli", clientSecret)
+  val keycloakConfig = KeycloakConfig("http", "127.0.0.1", 8080, "master", authConfig)
+
+  /* Keycloak Client Implicits **/
+  implicit val context: ContextShift[IO] = IO.contextShift(global)
+  implicit val backend: SttpBackend[IO, Nothing] = ServerInitializer.backend
+  implicit val client: KeycloakClient[IO, Nothing] = new KeycloakClient[IO, Nothing](keycloakConfig)
+
+  /* Keycloak Services **/
+  val userService: Users[IO, Nothing] = Keycloak.Users[IO, Nothing]
+  val groupService: Groups[IO, Nothing] = Keycloak.Groups[IO, Nothing]
+  val identityProviderService: IdentityProviders[IO, Nothing] = Keycloak.IdentityProviders[IO, Nothing]
+
+  /* Test Helper Functions **/
+  def isSuccessful[A](response: Either[KeycloakError, A]): Assertion =
+    response shouldBe a [scala.util.Right[_, _]]
+}
