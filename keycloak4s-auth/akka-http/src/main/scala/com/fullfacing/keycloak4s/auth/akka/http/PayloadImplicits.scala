2e6cd4d082ffc560954b67d76128f5ae85716c72
Louis Muller <lmuller@fullfacing.com>
2019-06-05 10:49:33 +0200

    Expanded on PayloadImplicits.

--- keycloak4s-auth/akka-http/src/main/scala/com/fullfacing/keycloak4s/auth/akka/http/PayloadImplicits.scala
+++ keycloak4s-auth/akka-http/src/main/scala/com/fullfacing/keycloak4s/auth/akka/http/PayloadImplicits.scala
@@ -8,20 +8,42 @@ import com.fullfacing.keycloak4s.core.serialization.JsonFormats.default
 import org.json4s.Formats
 import org.json4s.jackson.Serialization.read
 
+/**
+ * Provides helper functions to safely extract values from any Payload object.
+ */
 object PayloadImplicits {
+
+  /* A safe extraction method to extract any field's value from a Payload. **/
   private def safeExtract(payload: Payload, key: String): Option[String] = Try {
     payload.toJSONObject.getAsString(key)
   }.toOption
 
   implicit class PayloadImpl(payload: Payload) {
+
+    /* Generic Extractors. **/
+
     def extract(key: String): Option[String] =
       safeExtract(payload, key)
+
+    def extractList(key: String): List[String] =
+      safeExtract(payload, key).map(read[List[String]](_)).getOrElse(List.empty[String])
 
     def extractAs[A : Manifest](key: String)(implicit formats: Formats = default): Option[A] =
       safeExtract(payload, key).map(read[A](_)(formats, manifest))
 
+    def extractAsListOf[A : Manifest](key: String)(implicit formats: Formats = default): List[A] =
+      safeExtract(payload, key).map(read[List[A]](_)(formats, manifest)).getOrElse(List.empty[A])
+
+    /* User Info Extractors **/
+
     def extractEmail: Option[String] =
       safeExtract(payload, "email")
+
+    def extractEmailVerified: Option[Boolean] =
+      extractAs[Boolean]("email_verified")
+
+    def extractUsername: Option[String] =
+      safeExtract(payload, "preferred_username")
 
     def extractFirstName: Option[String] =
       safeExtract(payload, "given_name")
@@ -32,10 +54,21 @@ object PayloadImplicits {
     def extractFullName: Option[String] =
       safeExtract(payload, "name")
 
-    def extractUsername: Option[String] =
-      safeExtract(payload, "preferred_username")
+    /* Access Control Extractors **/
 
-    def extractResources: Map[String, ResourceRoles] =
+    def extractResourceAccess: Map[String, ResourceRoles] =
       extractAs[Map[String, ResourceRoles]]("resource_access").getOrElse(Map.empty[String, ResourceRoles])
+
+    def extractResourceRoles(resource: String): List[String] =
+      extractResourceAccess.get(resource).fold(List.empty[String])(_.roles)
+
+    def extractRealmAccess: Option[ResourceRoles] =
+      extractAs[ResourceRoles]("realm_access")
+
+    def extractRealmRoles: List[String] =
+      extractRealmAccess.fold(List.empty[String])(_.roles)
+
+    def extractScopes: List[String] =
+      extract("scope").fold(List.empty[String])(_.split(" ").toList)
   }
 }
--- keycloak4s-auth/akka-http/src/main/scala/com/fullfacing/keycloak4s/auth/akka/http/directives/ValidationDirective.scala
+++ keycloak4s-auth/akka-http/src/main/scala/com/fullfacing/keycloak4s/auth/akka/http/directives/ValidationDirective.scala
@@ -7,8 +7,6 @@ import com.fullfacing.keycloak4s.auth.akka.http.PayloadImplicits._
 import com.fullfacing.keycloak4s.auth.akka.http.models.AuthPayload
 import com.fullfacing.keycloak4s.auth.akka.http.services.TokenValidator
 import com.fullfacing.keycloak4s.core.models.KeycloakException
-import com.fullfacing.keycloak4s.core.serialization.JsonFormats.default
-import org.json4s.jackson.Serialization.write
 
 import scala.util.{Failure, Success}
 
@@ -41,7 +39,7 @@ trait ValidationDirective {
 
   /** Handles the success/failure of the token validation. */
   private def handleValidationResponse(response: Either[KeycloakException, AuthPayload]): Directive1[AuthPayload] = response match {
-    case Right(r) => provide(r.copy(resourceRoles = r.accessToken.extractResources))
+    case Right(r) => provide(r.copy(resourceRoles = r.accessToken.extractResourceAccess))
     case Left(t)  => complete(HttpResponse(status = t.code, entity = HttpEntity(ContentTypes.`text/plain(UTF-8)`, t.getMessage)))
   }
 }
