2bcf68ec65ae5cf145a293d9a03cd1b845b3c8b9
Stuart Jameson <sjameson@fullfacing.com>
2019-06-05 09:45:46 +0200

    Made MethodRoles a case class

--- keycloak4s-auth/akka-http/src/main/scala/com/fullfacing/keycloak4s/auth/akka/http/models/MethodRoles.scala
+++ keycloak4s-auth/akka-http/src/main/scala/com/fullfacing/keycloak4s/auth/akka/http/models/MethodRoles.scala
@@ -2,14 +2,10 @@ package com.fullfacing.keycloak4s.auth.akka.http.models
 
 import com.fullfacing.keycloak4s.core.models.enums.Method
 
-class MethodRoles(val method: Method,
-                  val roles: List[String]) {
+case class MethodRoles(method: Method,
+                       roles: List[String]) {
 
   def evaluateUserAccess(userRoles: List[String]): Boolean = {
     roles.intersect(userRoles).nonEmpty
   }
-}
-
-object MethodRoles {
-  def apply(method: Method, roles: List[String]): MethodRoles = new MethodRoles(method, roles)
 }
