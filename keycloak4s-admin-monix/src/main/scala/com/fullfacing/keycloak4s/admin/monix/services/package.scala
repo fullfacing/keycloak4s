3863a2175c43268c4eca4a8fff185768433588f7
mark@vectos.net <mark@vectos.net>
2019-11-15 11:05:28 +0100

    Support 2.12

--- keycloak4s-admin-monix/src/main/scala/com/fullfacing/keycloak4s/admin/monix/services/package.scala
+++ keycloak4s-admin-monix/src/main/scala/com/fullfacing/keycloak4s/admin/monix/services/package.scala
@@ -7,10 +7,10 @@ import scala.collection.immutable.Seq
 
 package object services {
 
-  def createQuery(queries: (String, Option[Any])*): Seq[KeyValue] = Seq.from {
+  def createQuery(queries: (String, Option[Any])*): Seq[KeyValue] = Seq {
     queries.flatMap { case (key, value) =>
       value.map(v => KeyValue(key, v.toString))
-    }
+    }:_*
   }
 
   def toCsvList(list: Option[List[String]]): Option[String] = list.map(_.mkString(","))
