72fd7a890f8fbc082ccef295d09e83af140ec23d
stuartjameson <sjameson@fullfacing.com>
2020-04-09 10:12:30 +0200

    _

--- keycloak4s-playground/src/main/scala/com/fullfacing/transport/Main.scala
+++ keycloak4s-playground/src/main/scala/com/fullfacing/transport/Main.scala
@@ -24,15 +24,18 @@ object Main extends TaskApp {
    * Config details of the Keycloak server to allow for Admin API calls.
    * Replace with applicable details.
    */
-  val host: String        = "localhost"
-  val port: Int           = 8080
-  val targetRealm: String = "master" //Name of a Realm whose data will be accessed/manipulated.
-  val adminRealm: String  = "master" //Name of a Realm with admin rights that can access/manipulate targetRealm.
-  val adminClient: String = "admin-cli" //Name of the admin Client inside adminRealm.
-  val adminSecret: String = "???" //Secret of adminClient.
+  lazy val config: ConfigWithAuth = {
+    val host: String        = "localhost"
+    val port: Int           = 8080
+    val targetRealm: String = "master" //Name of a Realm whose data will be accessed/manipulated.
+    val adminRealm: String  = "master" //Name of a Realm with admin rights that can access/manipulate targetRealm.
+    val adminClient: String = "admin-cli" //Name of the admin Client inside adminRealm.
+    val adminSecret: String = "???" //Secret of adminClient.
 
-  val authConfig  = KeycloakConfig.Secret(adminRealm, adminClient, adminSecret)
-  val config      = ConfigWithAuth("http", host, port, targetRealm, authConfig)
+    val authConfig  = KeycloakConfig.Secret(adminRealm, adminClient, adminSecret)
+
+    ConfigWithAuth("http", host, port, targetRealm, authConfig)
+  }
 
   def run(args: List[String]): Task[ExitCode] = Akka.connect().flatMap { _ =>
 
